97ff2e4b92e5d8b4fb791e8b7850b4177ba3587f
Thomas Girard <thomas.girard12@gmail.com>
2018-02-01 11:56:39 +0100

    Remove allready move test

--- src/test/kotlin/com/tgirard12/graphqlkotlindsl/SchemaDslTest.kt
+++ src/test/kotlin/com/tgirard12/graphqlkotlindsl/SchemaDslTest.kt
@@ -1,8 +1,5 @@
 package com.tgirard12.graphqlkotlindsl
 
-import com.tgirard12.graphqlkotlindsl.models.ListTypes
-import com.tgirard12.graphqlkotlindsl.models.SimpleEnum
-import com.tgirard12.graphqlkotlindsl.models.SimpleTypes
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.specs.WordSpec
 import org.junit.runner.RunWith
@@ -15,76 +12,8 @@ class SchemaDslTest : WordSpec() {
             "with empty schema" {
                 schemaDsl { } schemaEqual """
 schema {
-$TAB
-$TAB
-}"""
-            }
-        }
-        "SchemaDslTest query" should {
-        }
-        "SchemaDslTest mutation" should {
-        }
-        "SchemaDslTest type" should {
-            "print simple Type" {
-                schemaDsl {
-                    type<SimpleTypes> { }
-                } schemaEqual """
-schema {
-$TAB
-$TAB
 }
-
-
-
-
-type SimpleTypes {
-    double: Double!
-    doubleNull: Double
-    float: Float!
-    floatNull: Float
-    int: Int!
-    intNull: Int
-    long: Long!
-    longNull: Long
-    string: String!
-    stringNull: String
-    uuid: UUID!
-    uuidNull: UUID
-}"""
-            }
-            "print List types" {
-                schemaDsl {
-                    type<ListTypes> { }
-                } schemaEqual """
-schema {
-$TAB
-$TAB
-}
-
-
-
-
-type ListTypes {
-    ints: [Int]!
-    intsNull: [Int]
-}"""
-            }
-            "print enum" {
-                schemaDsl {
-                    enum<SimpleEnum> { }
-                } schemaEqual """
-schema {
-$TAB
-$TAB
-}
-
-
-
-enum SimpleEnum {
-    val1
-    VAL_2
-    enum
-}"""
+"""
             }
         }
     }
