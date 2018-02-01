c02b7ddffff660119e48f10bda87eb3cf679e4a2
Thomas Girard <thomas.girard12@gmail.com>
2018-02-01 11:56:10 +0100

    uuid scalar

---
+++ src/main/kotlin/com/tgirard12/graphqlkotlindsl/graphqljava/GqlJavaScalars.kt
@@ -0,0 +1,32 @@
+package com.tgirard12.graphqlkotlindsl.graphqljava
+
+import graphql.schema.CoercingSerializeException
+import java.util.*
+
+
+class GqlJavaScalars {
+    companion object {
+
+        val uuid by lazy {
+            GqlJavaExtentions.scalarTypeDsl<UUID> {
+                parseLiteral {
+                    TODO()
+                }
+                parseValue {
+                    when (it) {
+                        is String -> UUID.fromString(it)
+                        else -> throw CoercingSerializeException("parseValue expected type 'UUID' " +
+                                "but was ${it?.javaClass?.simpleName ?: "NULL"}")
+                    }
+                }
+                serialize {
+                    when (it) {
+                        is String -> it.toString()
+                        else -> throw CoercingSerializeException("serialize expected type 'UUID' " +
+                                "but was ${it?.javaClass?.simpleName ?: "NULL"}")
+                    }
+                }
+            }
+        }
+    }
+}
