7d9626e85006430589544d034839746a5cbb2f7d
Thomas Girard <thomas.girard12@gmail.com>
2018-02-02 14:06:36 +0100

    Readme documentation

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 # graphql-kotlin-dsl
-Kotlin DSL to generate GraphQl Schema
+
+Kotlin DSL to generate GraphQL Schema
+
+- Use a DSL to easily generate your GraphQL Schema (IDL)
+- Generate a GraphQLSchema with the graphql-java library
+- Kotlin DSL to define custom scalars
+
+
+## GraphQLSchema
+
+```kotlin
+val schema: GraphQLSchema = schemaDsl {
+
+  // ... schema definition
+
+}.graphQLSchema(myRuntimeWiring)
+```
+
+## Scalar DSL
+
+```kotlin
+val scalar = scalarTypeDsl<LocalDateTime> {
+  serialize {
+    // ...
+  }
+  parseValue {
+    // ...
+  }
+  parseLiteral {
+    // ...
+  }
+}
+
+```
+
+## Schema (IDL)
+
+```kotlin
+data class User(
+  val name: String, 
+  val email: String
+)
+
+enum class Right {
+    read, write, execute
+}
+
+schemaDsl {
+ 
+  // Scalar
+  scalar<Double>()
+  scalar<UUID>()
+
+  // Types and Enums
+  type<User>()
+  enum<Right>
+
+  // Queries
+  query<User> {
+    arg<UUID> { name = "id" }
+  }
+  query<Unit> {
+    name = "users"
+    returnType = "[User]"
+  }
+
+  // Mutations
+  mutation<User> {
+      name = "updateUser"
+
+      arg<Int> { 
+        name = "count"
+        nullable = true
+      }
+      arg<String> { name = "name" }
+  } 
+}
+```
+
+Generate this GraphQl schema :  
+
+```
+schema {
+    query: QueryType
+    mutation: MutationType
+}
+
+type QueryType {
+    user(id: UUID!): User!
+    users: [User]!
+}
+
+type MutationType {
+    updateUser(count: Int, name: String!): User!
+}
+
+scalar Double
+scalar UUID
+
+enum Right {
+    read
+    write
+    execute
+}
+
+type User {
+    email: String!
+    name: String!
+}
+```
---
+++ src/test/kotlin/com/tgirard12/graphqlkotlindsl/ReadmeTest.kt
@@ -0,0 +1,77 @@
+package com.tgirard12.graphqlkotlindsl
+
+import com.tgirard12.graphqlkotlindsl.models.Right
+import com.tgirard12.graphqlkotlindsl.models.User
+import io.kotlintest.KTestJUnitRunner
+import io.kotlintest.specs.WordSpec
+import org.junit.runner.RunWith
+import java.util.*
+
+@RunWith(KTestJUnitRunner::class)
+class ReadmeTest : WordSpec() {
+
+    init {
+        "readme" should {
+            "valid README Sample" {
+                schemaDsl {
+                    // Scalar
+                    scalar<Double>()
+                    scalar<UUID>()
+
+                    // Types and Enums
+                    type<User> { }
+                    enum<Right> { }
+
+                    // Queries
+                    query<User> {
+                        arg<UUID> { name = "id" }
+                    }
+                    query<Unit> {
+                        name = "users"
+                        returnType = "[User]"
+                    }
+
+                    // Mutations
+                    mutation<User> {
+                        name = "updateUser"
+
+                        arg<Int> {
+                            name = "count"
+                            nullable = true
+                        }
+                        arg<String> { name = "name" }
+                    }
+                } schemaEqual """
+schema {
+    query: QueryType
+    mutation: MutationType
+}
+
+type QueryType {
+    user(id: UUID!): User!
+    users: [User]!
+}
+
+type MutationType {
+    updateUser(count: Int, name: String!): User!
+}
+
+scalar Double
+scalar UUID
+
+enum Right {
+    read
+    write
+    execute
+}
+
+type User {
+    email: String!
+    name: String!
+}
+"""
+            }
+
+        }
+    }
+}
