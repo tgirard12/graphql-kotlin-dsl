4c823226a2e81c6db3f5607b04735815b8e14284
Thomas Girard <thomas.girard12@gmail.com>
2018-03-27 13:58:55 +0200

    Update Readme with dataFetcher DSL

--- src/test/kotlin/com/tgirard12/graphqlkotlindsl/ReadmeTest.kt
+++ src/test/kotlin/com/tgirard12/graphqlkotlindsl/ReadmeTest.kt
@@ -1,7 +1,14 @@
 package com.tgirard12.graphqlkotlindsl
 
+import Stubs
+import com.tgirard12.graphqlkotlindsl.graphqljava.GqlJavaScalars
+import com.tgirard12.graphqlkotlindsl.graphqljava.asyncDataFetcher
+import com.tgirard12.graphqlkotlindsl.graphqljava.graphQL
+import com.tgirard12.graphqlkotlindsl.graphqljava.staticDataFetcher
 import com.tgirard12.graphqlkotlindsl.models.Right
 import com.tgirard12.graphqlkotlindsl.models.User
+import graphql.GraphQLError
+import graphql.execution.AsyncExecutionStrategy
 import io.kotlintest.KTestJUnitRunner
 import io.kotlintest.specs.WordSpec
 import org.junit.runner.RunWith
@@ -10,49 +17,69 @@ import java.util.*
 @RunWith(KTestJUnitRunner::class)
 class ReadmeTest : WordSpec() {
 
+    private val schemaDsl = schemaDsl {
+        // Scalar
+        scalar<Double> { GqlJavaScalars.double }
+        scalar<UUID> { GqlJavaScalars.uuid }
+
+        // Types and Enums
+        type<User>(typeDescription = "An User") {
+            desc("email", "User Email")
+
+            addField<String>(name = "otherName") {
+                asyncDataFetcher<String>("otherName") {
+                    "MyOtherName"
+                }
+            }
+            addField<Right> {
+                description = "User Right"
+                nullable = true
+                asyncDataFetcher<Right>("right") {
+                    Right.execute
+                }
+            }
+
+            dropField("deleteField")
+        }
+        enum<Right>(enumDescription = "An enum") { }
+
+        // Queries
+        query<User>(queryDescription = "User By Id") {
+            arg<UUID> { name = "id" }
+
+            asyncDataFetcher { env ->
+                Stubs.users.firstOrNull { it.id == env.arguments["id"] }
+            }
+        }
+        query<Unit> {
+            name = "users"
+            description = "All Users"
+            returnType = "[User]"
+
+            staticDataFetcher {
+                Stubs.users
+            }
+        }
+
+        // Mutations
+        mutation<User>(mutationDescription = "Update a user") {
+            name = "updateUser"
+
+            arg<Int>("count") {
+                nullable = true
+            }
+            arg<String> { name = "name" }
+
+            asyncDataFetcher { env ->
+                User(UUID.randomUUID(), env.arguments["name"] as String, "email@gql.io", 5)
+            }
+        }
+    }
+
     init {
         "readme" should {
-            "valid README Sample" {
-                schemaDsl {
-                    // Scalar
-                    scalar<Double>()
-                    scalar<UUID>()
-
-                    // Types and Enums
-                    type<User>(typeDescription = "An User") {
-                        desc("email", "User Email")
-
-                        addField<String>(name = "otherName") { }
-                        addField<Right> {
-                            description = "User Right"
-                            nullable = true
-                        }
-
-                        dropField("deleteField")
-                    }
-                    enum<Right>(enumDescription = "An enum") { }
-
-                    // Queries
-                    query<User>(queryDescription = "User By Id") {
-                        arg<UUID> { name = "id" }
-                    }
-                    query<Unit> {
-                        name = "users"
-                        description = "All Users"
-                        returnType = "[User]"
-                    }
-
-                    // Mutations
-                    mutation<User>(mutationDescription = "Update a user") {
-                        name = "updateUser"
-
-                        arg<Int> {
-                            name = "count"
-                            nullable = true
-                        }
-                        arg<String> { name = "name" }
-                    }
-                } schemaEqual """
+            "valid README Schema Sample" {
+                schemaDsl schemaEqual """
 schema {
     query: QueryType
     mutation: MutationType
@@ -94,6 +121,32 @@ type User {
 """
             }
 
+            "valid README DataFectcherDsl" {
+                val graphQL = schemaDsl.graphQL {
+                    queryExecutionStrategy(AsyncExecutionStrategy())
+                }
+
+                val queryRes = graphQL.execute("""
+                    |query user {
+                    |   user(id: "b6214ea0-fc5a-493c-91ea-939e17b2e95f") {
+                    |       id
+                    |       email
+                    |       name
+                    |       otherName
+                    |       right
+                    |   }
+                    |}""".trimMargin())
+                queryRes.errors shouldBe listOf<GraphQLError>()
+                queryRes.getData<Map<String, Map<String, Any>>>() shouldBe hashMapOf(
+                        "user" to hashMapOf(
+                                "id" to "b6214ea0-fc5a-493c-91ea-939e17b2e95f",
+                                "email" to "john@mail.com",
+                                "name" to "John",
+                                "right" to Right.execute.name,
+                                "otherName" to "MyOtherName"
+                        )
+                )
+            }
         }
     }
 }
